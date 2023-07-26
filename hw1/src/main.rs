790e4cf1741b1162e115a75cc6e6cab8949f2529
Sami Sezgin <sezginsami@gmail.com>
2023-07-27 02:20:11 +0300

    target folder deleted

--- hw1/src/main.rs
+++ hw1/src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
     let string1: &str = "Rust Bootcamp";
-    let string2: &str = " Birinci Ödev Çözümü";
+    let string2: &str = " Solution of HW #1";
     let concatenated_string = concatenate_strings(string1, string2);
 
     println!("The value of concatenated_string is {}",concatenated_string);    
 }
 
 fn concatenate_strings(string1: &str, string2: &str) -> String {
-    let mut result: String = String::from("");
-
-    result.push_str(string1);
+    let mut result: String = String::from(string1); 
     result.push_str(string2);
     result
 }
